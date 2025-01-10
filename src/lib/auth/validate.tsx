da7de72583e20018009a4e62038acc312e091264
Marcio Barrientos <marcio.barrientos@gmail.com>
2025-01-10 00:33:29 -0600

    Temporal redirect to handle empty sesion

--- src/lib/auth/validate.tsx
+++ src/lib/auth/validate.tsx
@@ -2,12 +2,14 @@
 
 import { signOut, useSession } from "next-auth/react"
 import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Validate({ children }: { children: React.ReactNode }) {
   const { data: session, update } = useSession()
+  const router = useRouter()
   useEffect(() => {
     if (session?.tokenIsRefreshed) {
-      update({
+      void update({
         access_token: session.access_token,
         expires_at: session.expires_at,
         refresh_token: session.refresh_token,
@@ -15,9 +17,15 @@ export default function Validate({ children }: { children: React.ReactNode }) {
       })
     }
     if (session?.error && session.error === "RefreshAccessTokenError") {
-      signOut()
+      signOut().then(() => {
+        router.push("/")
+      })
     }
-  }, [session, update])
+    // TODO: Move this feature to middleware
+    if (!session) {
+      router.push("/")
+    }
+  }, [session, update, router])
 
   return children
 }
